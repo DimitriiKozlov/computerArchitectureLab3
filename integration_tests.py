fc0b628291f80e2aa042f5063ef8f6c7908e7c85
Dimitrii <ikzdimitrij@gmail.com>
2015-06-25 20:55:23 +0300

    v3Test

--- integration_tests.py
+++ integration_tests.py
@@ -5,24 +5,18 @@ from operations import create, read, read_all, update, delete
 
 
 class IntegrationTests(unittest.TestCase):
-    """@mock.patch("mypackage.func")
-    def test_createdb(self, mock_func):
-        create_db('4')
-        assert mock_func.call_args == [call('a')]"""
-
-
     def test_create(self):
         self.assertEqual(create('nam1', 'surnam1'), "new doc created")
 
     def test_read_all(self):
         self.assertEqual(read_all(), [
-            "<Document u'16f98a5c35dcb61ce73f486390005be5'@u'2-314ae555d881c79e88a21d4df5230f72' {u'surname': u'fg', u'name': u'gf'}>",
-            "<Document u'16f98a5c35dcb61ce73f486390006990'@u'1-138b49b469c90bc3a5c734c06461ede3' {u'surname': u'text', u'name': u'text'}>",
-            "<Document u'16f98a5c35dcb61ce73f486390006dc9'@u'1-61c91e79536b7c0562503ca084c912b8' {u'surname': u'we', u'name': u'we'}>]"])
+            "<Document u'92ced8f9b1ad927a138414862d000487'@u'1-eb4055c4818429e8b4c4829d845e4d9e' {u'surname': u'h', u'name': u'D'}>",
+            "<Document u'92ced8f9b1ad927a138414862d0020e6'@u'1-f74b26f6c09af40d585e365d0e552b08' {u'surname': u'jkl56', u'name': u'ASS124'}>",
+            "<Document u'92ced8f9b1ad927a138414862d0030ba'@u'1-6bfead417512ef01d2a176c7a34101cd' {u'surname': u'tEST', u'name': u'Test'}>"])
 
     def test_read(self):
-        self.assertEqual(read(0),
-                         "<Document u'16f98a5c35dcb61ce73f486390005be5'@u'2-314ae555d881c79e88a21d4df5230f72' {u'surname': u'fg', u'name': u'gf'}>")
+        self.assertEqual(read(2),
+                         "<Document u'92ced8f9b1ad927a138414862d0030ba'@u'1-6bfead417512ef01d2a176c7a34101cd' {u'surname': u'tEST', u'name': u'Test'}>")
 
     def test_update(self):
         self.assertEqual(update(1, 'newdata1', 'newdata2'), "updated")
